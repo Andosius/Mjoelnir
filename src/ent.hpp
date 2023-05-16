22b28930085192d78120bc17324353203f625664
Denis Ahmatovic <37641046+Andosius@users.noreply.github.com>
2023-05-16 23:10:58 +0200

    Add better offset information to the script

--- src/ent.hpp
+++ src/ent.hpp
@@ -7,7 +7,7 @@ class weaponinfo
 {
 public:
 	char FILLER_1[288];
-	uint16_t w_RecoilSeed[2];
+	uint16_t w_RecoilSeed[2]; // Offset: 288 (0x0120)
 };
 
 class ent;
@@ -16,59 +16,36 @@ class weapon
 {
 public:
 	char FILLER_1[4];
-	uint32_t w_ID; //0x0004
-	ent* w_Owner; //0x0008
-	weaponinfo* w_Info; //0x000C
-	int* w_SpareAmmo; //0x0010
-	int* w_CurrentAmmo; //0x0014
+	uint32_t w_ID; // Offset: 4 (0x0004)
+	ent* w_Owner; // Offset: 8 (0x0008)
+	weaponinfo* w_Info; // Offset: 12 (0x000C)
+	int* w_SpareAmmo; // Offset: 16 (0x0010)
+	int* w_CurrentAmmo; // Offset: 20 (0x0014)
 };
 
 
 class ent
 {
-public:	
+public:
 	char FILLER_1[4];
-	glm::vec3 m_HeadPosition; //0x0004
-	glm::vec3 m_Velocity; //0x0010
+	glm::vec3 m_HeadPosition; // Offset: 4 (0x0004)
+	glm::vec3 m_Velocity; // Offset: 16 (0x0010)
 	char FILLER_2[24];
-	glm::vec3 m_Position; //0x0034
-	glm::vec3 m_HeadAngle; //0x0040
+	glm::vec3 m_Position; // Offset: 52 (0x0034)
+	glm::vec3 m_HeadAngle; // Offset: 64 (0x0040)
 	char FILLER_3[37];
-	bool m_IsAiming; //0x0071
+	bool m_IsAiming; // Offset: 113 (0x0071)
 	char FILLER_4[134];
-	uint32_t m_Health; //0x00F8
-	uint32_t m_Armor; //0x00FC
+	uint32_t m_Health; // Offset: 248 (0x00F8)
+	uint32_t m_Armor; // Offset: 252 (0x00FC)
 	char FILLER_5[292];
-	bool m_Attacking;
-	char m_Username[16]; //0x0225
+	bool m_Attacking; // Offset: 548 (0x0224)
+	char m_Username[16]; // Offset: 565 (0x0235)
 	char FILLER_6[247];
-	int8_t m_Team; //0x032C
+	int8_t m_Team; // Offset: 820 (0x0334)
 	char FILLER_7[11];
-	uint32_t m_State; //0x0338
+	uint32_t m_State; // Offset: 840 (0x0348)
 	char FILLER_8[56];
-	weapon* m_Weapon; //0x0374
+	weapon* m_Weapon; // Offset: 884 (0x0374)
 	char FILLER_9[520];
-	// WORKING!
-	/*
-	char FILLER_1[4];
-	vec3 m_HeadPosition; //0x0004
-	vec3 m_Velocity; //0x0010
-	char FILLER_2[24];
-	vec3 m_Position; //0x0034
-	vec3 m_HeadAngle; //0x0040
-	char FILLER_3[37];
-	BYTE m_IsAiming; //0x0071
-	char FILLER_4[134];
-	__int32 m_Health; //0x00F8
-	__int32 m_Armor; //0x00FC
-	char FILLER_5[292];
-	BYTE m_Attacking;
-	char m_Username[16]; //0x0225
-	char FILLER_6[247];
-	BYTE m_Team; //0x032C
-	char FILLER_7[11];
-	BYTE m_State; //0x0338
-	char FILLER_8[59];
-	weapon* m_Weapon; //0x0374
-	*/
 };
